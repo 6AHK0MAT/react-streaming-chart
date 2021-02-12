a6a18879cc33739a95daf9b1d442df89f6374f83
vladimir.makhmutov <vladimir.makhmutov@indusoft.ru>
2021-02-12 12:34:39 +0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
+import 'chartjs-plugin-streaming';
 import './App.css';
+import greeterApi from './util/greeterApi';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    streamF = (chart) => {
+        console.log("Start streaming");
+        let name = '';
+        // streaming call
+        const stream = greeterApi.sayRepeatHello({ name, count: 1 });
+        stream.on("data", (response) => {
+            let chartArrText = ''
+            chartArrText = response.getMessage();
+            let chartArr = chartArrText.split(',').map(Number)
+            console.log('Получаем данные...');
+            console.log(chartArr);
+            chart.config.data.datasets.forEach(function(dataset) {
+                dataset.data.push({
+                    x: Date.now(),
+                    y: chartArr[0]
+                });
+            });
+
+        });
+        stream.on("error", (err) => {
+            console.log(
+                `Unexpected stream error: code = ${err.code}` +
+                `, message = "${err.message}"`
+            );
+        });
+        return () => {
+            console.log("Stop streaming");
+        };
+    }
+
+    onRefresh = (chart) => {
+        chart.config.data.datasets.forEach(function(dataset) {
+            dataset.data.push({
+                x: Date.now(),
+                y: (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100)
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Line
+                data={{
+                    datasets: [{
+                        label: 'Данные установки',
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        fill: false,
+                        cubicInterpolationMode: 'monotone',
+                        data: []
+                    }]
+                }}
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Пример графика с данными gRCP'
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: 'realtime',
+                            realtime: {
+                                duration: 20000,
+                                refresh: 1000,
+                                delay: 2000,
+                                onRefresh: this.streamF
+                            }
+                        }],
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'value'
+                            }
+                        }]
+                    },
+                    tooltips: {
+                        mode: 'nearest',
+                        intersect: false
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: false
+                    }
+                }}
+            />
+        );
+    }
 }
 
 export default App;
---
+++ src/util/greeterApi.js
@@ -0,0 +1,21 @@
+import { GreeterClient } from '../grpc/helloworld_grpc_web_pb.js';
+import { HelloRequest, RepeatHelloRequest } from '../grpc/helloworld_pb.js';
+
+const URL = "http://" + window.location.hostname + ":8088";
+const client = new GreeterClient(URL, null, null);
+
+const metadata = { 'custom-header-1': 'value1' };
+
+export default {
+  sayHello: (name, callback) => {
+    const request = new HelloRequest();
+    request.setName(name);
+    client.sayHello(request, metadata, callback);
+  },
+  sayRepeatHello: ({ name, count }) => {
+    const streamRequest = new RepeatHelloRequest();
+    streamRequest.setName(name);
+    streamRequest.setCount(count);
+    return client.sayRepeatHello(streamRequest, metadata);
+  },
+};
